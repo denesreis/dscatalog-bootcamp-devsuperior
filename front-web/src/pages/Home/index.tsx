d73243c4152fbc55db03a3e9e54a61a305426898
“Denes <denesreis@scasistemas.com.br>
2021-03-10 09:05:10 -0300

    Finalizando a pagina home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import './styles.scss';
+import {ReactComponent as MainImage} from '../../core/assets/images/main-image.svg';
+import ButtonIcon from '../../core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 
+/*.home-content+tab cria a classe */
 const Home = () => (
-    <div>
-        <h1>Home</h1>
+    <div className="home-container"> 
+        <div className="row home-content">
+            <div className="col-6 home-text">
+            <h1 className="text-title">Conheça o melhor <br /> catálogo de produtos</h1>
+            <p className="text-subtitle">
+                Ajudaremos você a encontrar os melhores  <br /> produtos disponíveis no mercado.
+            </p>
+            <Link to="/Catalog">
+                <ButtonIcon text="inicie agora a sua busca" />
+            </Link>
+            </div>
+            <div className="col-6">
+                <MainImage className="main-image" />
+            </div>
+
+        </div>
+        
     </div>
 );
 
