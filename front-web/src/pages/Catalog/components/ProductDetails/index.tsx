99bb62ec33e77fc282a27cbbb21da4bc214d0b04
“Denes <denesreis@scasistemas.com.br>
2021-03-18 10:07:30 -0300

    Criando Conteudo dos Detalhes

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,5 +1,7 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './styles.scss';
+import {ReactComponent as ArrowIcon} from '../../../../core/assets/images/arrow.svg'
+import {ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg'
 
 type ParamsType = {
     productId : string;
@@ -7,12 +9,46 @@ type ParamsType = {
 
 const ProductDetails = () => {
     const {productId} = useParams<ParamsType>();
-
+    console.log('Produto:'+productId);
+    /*row = classe do bootstrap e já tem um display flex embutido
+    text-center= classe do bootstrap 
+    Quando é colocado col-6 significa que vai ocupar metade da tela porque o bootstrap separa a tela em 12
+    pr-5 - padding right do bootstrap*/
     return (
         <div className = "product-details-container">
-            <h1>
-                Destalhes do produto
-            </h1>
+            <div className="card-base border-radius-20 product-details">
+                <Link to='/products' className="product-details-goback">
+                    <ArrowIcon className="icon-goback"/>
+                    <h1 className="text-goback">voltar</h1>  
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        <div className="product-details-card text-center">
+                            <ProductImage className="product-details-image" / >
+                        </div>
+                        <h1 className="product-details-name">
+                        Computador Desktop - Intel Core i7
+                        </h1>
+                     </div>
+                    <div className="col-6 product-details-card">
+                        <h1 className="product-description-title">
+                            Descrição do Produto
+                        </h1>
+                        <p className="product-description-text">
+                        Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos 
+                        simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais
+                         para obter mais espaço e trabalhar com os itens que você deseja. Além disso, 
+                         todas as notificações e principais configurações são reunidas em uma única tela 
+                         de fácil acesso.
+                        </p>
+                    </div>
+
+                </div>
+
+                
+      
+            </div>
+
 
         </div>
     )
